5a9f3e9ae8df6bf116d00688a665903b844a9b3b
Dustin Hoffman <dustin.hoffman@breefield.com>
2011-02-12 19:38:47 -0700

    Input area

--- js/script.js
+++ js/script.js
@@ -1,26 +1,22 @@
-/* Author: 
-
-*/
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+$(document).ready(function() {
+    
+    var viewport_size = {width: 0, height: 0};
+    $(window).resize(function(e) { sizeViewport(); });
+    function sizeViewport() {
+        viewport_size = {width: $(window).width(), height: $(window).height()};
+        $('footer .content,#beats-input,#beats-input-display').css(
+            {'height':      viewport_size.height - 20,
+             'max-height':  viewport_size.height - 20}
+        );
+    }
+    
+    function initialize() {
+        sizeViewport();
+    }
+    
+    //$('#beats-input').bind('keydown', function() {
+    //    $('#beats-input-display').text($('#beats-input').val());
+    //});
+    
+    initialize();
+});
